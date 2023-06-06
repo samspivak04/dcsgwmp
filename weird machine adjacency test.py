b8735490078a4bae20275283dbab0141aeb39772
samspivak04 <97407434+samspivak04@users.noreply.github.com>
2023-06-06 11:25:00 -0700

    labeled edges

--- weird machine adjacency test.py
+++ weird machine adjacency test.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import networkx as nx
-G = nx.Graph()
+# G = nx.Graph()
 
 # [currentstate, testinginput]: nextstate
 # dictionarytime = {[0, 0]: 1, [1, 0]: 0, [2, 0]: 0, [0, 1]: 2, [1, 1]: 2, [2, 1]: 1}
@@ -11,14 +11,41 @@ G = nx.Graph()
 # H.add_node(1, color = 'yellow')
 # H.add_node(2, color = 'blue')
 
-edgelist = [(0, 1, {'color': 'red'}), (2, 0, {'color': 'yellow'}), (1, 2, {'color': 'blue'})]
-H = nx.DiGraph(edgelist)
-print(H.edges)
+nodenumberslist = []
+for i in range(1, 5):
+    nodenumberslist.append(i)
+
+newedgelist = []
+edgelabelsdictionary = {}
+z = len(set(nodenumberslist)) - 1
+a = nodenumberslist[z]
+b = nodenumberslist[0]
+tuple2 = (a, b)
+newedgelist.append(tuple2)
+tuple3 = a, "to", b
+# tuple4 = tuple2: tuple3
+edgelabelsdictionary[tuple2] = tuple3
+for h in range(len(nodenumberslist)):
+    if h + 1 != len(nodenumberslist):
+        q = nodenumberslist[h]
+        u = nodenumberslist[h + 1]
+        tuple1 = (q, u)
+        newedgelist.append(tuple1)
+        tuple5 = q, "to", u
+        edgelabelsdictionary[tuple1] = tuple5
+
+H = nx.DiGraph(newedgelist)
+
+# edgelist = [(0, 1, {'color': 'red'}), (2, 0, {'color': 'yellow'}), (1, 2, {'color': 'blue'})]
+# H = nx.DiGraph(edgelist)
+# print(H.edges)
 # H.add_edge()
 
 # H.add_edges_from(edgelist)
 
 # node_color = 'g'
 # edge_color = 'b'
+pos = nx.spring_layout(H)
+nx.draw_networkx_edge_labels(H, pos = pos, edge_labels = edgelabelsdictionary)
 nx.draw(H, with_labels=True)
 plt.show()
