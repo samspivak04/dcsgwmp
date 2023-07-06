2836be6594392100155cc8fd0b99060c33978fa6
samspivak04 <97407434+samspivak04@users.noreply.github.com>
2023-07-06 10:37:03 -0700

    generated graph on top of graph

--- weird state transitions.py
+++ weird state transitions.py
@@ -1,10 +1,30 @@
 # import packages
 import matplotlib.pyplot as plt
 import networkx as nx
+import copy
+
+
+# edge labels dictionary
+def edgelabelsfunction(H):
+    edgelabelsdict = {}
+    for nodestuple in H.edges():
+        (firstnode, secondnode) = nodestuple  # unpack nodestuple
+        edgelabelsdict[nodestuple] = str(firstnode) + " to " + str(secondnode)  # create labels and...
+        # add them to edge labels dictionary
+    return edgelabelsdict
+def unreachablenodescolors(H):
+    unreachablenodes = list(nx.isolates(H))  # make a list of the unreachable nodes
+    nodecolors = []  # make a list of the colors for all the nodes
+    for node in H.nodes():
+        if node in unreachablenodes:
+            nodecolors.append('green')  # make all unreachable nodes green
+        else:
+            nodecolors.append('tab:blue')  # make all reachable nodes blue
+    return nodecolors
 
 fileobject = open('path_data_weird_state_transitions.csv', 'w', encoding="utf-8")
 
-numberofnodes = 10
+numberofnodes = 13
 criticaledgeprobability = 1 / (numberofnodes - 1)
 
 
@@ -18,26 +38,51 @@ for probabilitydenominator in range(1, int(numberofnodes * 2)):  # create more a
 
     # generate erdos renyi graph
     H = nx.erdos_renyi_graph(numberofnodes, (1 / int(probabilitydenominator)), seed=5, directed=True)
+    # J = copy.deepcopy(H)
+    J = nx.erdos_renyi_graph(numberofnodes, criticaledgeprobability, seed=5, directed=True)
+    K = copy.deepcopy(H)
+    K.add_edges_from([e for e in J.edges])
+    # J.add_edges_from([e for e in S.edges])
 
-    # edge labels dictionary
-    edgelabelsdict = {}
-    for nodestuple in H.edges():
-        (firstnode, secondnode) = nodestuple  # unpack nodestuple
-        edgelabelsdict[nodestuple] = str(firstnode) + " to " + str(secondnode)  # create labels and...
-        # add them to edge labels dictionary
+    edgelabelsdictH = edgelabelsfunction(H)
+    edgelabelsdictJ = edgelabelsfunction(J)
 
-    unreachablenodes = list(nx.isolates(H))
-    nodecolors = []
-    for node in H.nodes():
-        if node in unreachablenodes:
-            nodecolors.append('green')
-        else:
-            nodecolors.append('tab:blue')
+    unreachablenodescolorsH = unreachablenodescolors(H)
+    unreachablenodesK = len(list(nx.isolates(H))) - len(list(nx.isolates(K)))
+    fileobject.write("The amount of previously unreachable nodes that are now reachable is " + str(unreachablenodesK) + ".\n")
+
+    # # method='bellman-ford'
+    # try:
+    #     pathlist = nx.shortest_path(H, source=0, target=1, weight=None)
+    # except nx.NetworkXNoPath:
+    #     print("new edge will be created then deleted")
+    #     fileobject.write("new edge will be created then deleted\n")
+    #     H.add_edge(0, 1)
+    #     print("The path from " + str(0) + " to " + str(1) + " is:")
+    #     fileobject.write("The path from " + str(0) + " to " + str(1) + " is:" + "\n")
+    #     for k in range(len(pathlist)):
+    #         if k + 1 != len(set(pathlist)):
+    #             print(str(pathlist[k]) + " to " + str(pathlist[k + 1]))
+    #             fileobject.write(str(pathlist[k]) + " to " + str(pathlist[k + 1]) + "\n")
+    #     H.remove_edge(int(0), int(1))
+    # else:
+    #     print("The path from " + str(0) + " to " + str(1) + " is:")
+    #     fileobject.write("The path from " + str(0) + " to " + str(1) + " is:" + "\n")
+    #     for k in range(len(pathlist)):
+    #         if k + 1 != len(set(pathlist)):
+    #             print(str(pathlist[k]) + " to " + str(pathlist[k + 1]))
+    #             fileobject.write(str(pathlist[k]) + " to " + str(pathlist[k + 1]) + "\n")
+
+    # path = dict(nx.all_pairs_shortest_path(H))
+    # print(path)
 
     # display everything
     pos = nx.spring_layout(H)
-    nx.draw(H, pos=pos, with_labels=True, node_color=nodecolors)
-    nx.draw_networkx_edge_labels(H, pos=pos, edge_labels=edgelabelsdict, label_pos=0.5, font_weight='bold')
+    nx.draw(J, pos=pos, with_labels=True, node_color=unreachablenodescolorsH, edge_color="green")
+    nx.draw_networkx_edge_labels(J, pos=pos, edge_labels=edgelabelsdictH, label_pos=0.5, font_weight='bold')
+    nx.draw(H, pos=pos, with_labels=True, node_color=unreachablenodescolorsH)
+    nx.draw_networkx_edge_labels(H, pos=pos, edge_labels=edgelabelsdictH, label_pos=0.5, font_weight='bold')
+
     plt.savefig("graph_weird_state_transitions_1over" + str(probabilitydenominator) + ".png")
     plt.show()
 
